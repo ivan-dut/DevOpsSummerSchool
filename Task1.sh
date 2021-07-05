461e98f47d96154d7050383ffa87fa8c72fb9426
ivan-dut <80826965+ivan-dut@users.noreply.github.com>
2021-07-05 19:22:38 +0300

    Create Task1.sh

---
+++ Task1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if id "$1" &>/dev/null; then
+    echo 'The user found:' $1
+    if [ -d "$2" ]; then
+        echo 'The directory exist:' $2
+        sudo chown -R $1 $2
+    else
+        echo 'The directory dosen`t exist:' $2
+    fi
+else
+    echo 'user not found:' $1
+fi
